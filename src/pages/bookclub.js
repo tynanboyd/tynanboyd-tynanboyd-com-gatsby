3d1a282afbd191ad27557196e60632b279a28c11
= Tynan Boyd <tynan@pixeldesigns.ca>
2021-05-07 15:11:43 -0600

    lets move the club

--- src/pages/bookclub.js
+++ src/pages/bookclub.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { Router } from "@reach/router";
-import PrivateRoute from "../components/PrivateRoute";
-import BookClubNavBar from "../components/BookClubNavBar";
+import React from 'react';
+{
+  /* import { Router } from "@reach/router";
+import PrivateRoute from "../components/PrivateRoute"; */
+}
+import BookClubNavBar from '../components/BookClubNavBar';
 
-import Profile from "../components/Profile";
-import Login from "../components/Login";
+{
+  /* import Profile from "../components/Profile"; */
+}
+{
+  /* import Login from "../components/Login"; */
+}
 const App = () => (
   <>
     <h1>No Dead White Men Bookclub</h1>
-    <Router>
+
+    {/* <Router>
       <PrivateRoute path="/bookclub/profile" component={Profile} />
       <Login path="/bookclub/login" />
-    </Router>
+    </Router> */}
+
     <BookClubNavBar />
   </>
 );
