4803d61d64a98d385879ea70769a42dad32d5df2
Tynan Boyd <tynanianboyd@gmail.com>
2021-05-08 07:59:51 -0600

    favicon

--- src/components/seo.js
+++ src/components/seo.js
@@ -24,7 +24,7 @@ export default function SEO({ children, location, description, title }) {
 		<title>{title}</title>
 		{/* Fav Icons */}
 		<link rel="icon" type="image/svg+xml" href="/favicon.png" />
-		<link rel="alternate icon" href="/favicon.ico" />
+		<link rel="alternate icon" href="/logo.svg" />
 		{/* Meta Tags */}
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta charSet="utf-8" />
